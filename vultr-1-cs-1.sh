397086b0fd9cf30664b50de3b9727c479da20312
phamhung140295 <phamhung140295@gmail.com>
2021-03-22 09:54:05 +0700

    Update vultr-1-cs-1.sh

--- vultr-1-cs-1.sh
+++ vultr-1-cs-1.sh
@@ -516,8 +516,7 @@ else
 fi
 
 # Post Content Profile to Server
-account_email="hungphamfw95@gmail.com"
 instance_name="vultr-1-cs-1"
 ip_address=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
 current_profile=$(base64 /root/openvpn.ovpn)
-curl -d "type=update&account_email=$account_email&instance_name=$instance_name&ip_address=$ip_address&current_profile=$current_profile" -X POST "https://air.xinstores.com/openvpn/api.php"
+curl -d "type=update&instance_name=$instance_name&ip_address=$ip_address&current_profile=$current_profile" -X POST "https://air.xinstores.com/openvpn/api.php"
